d232ffa7f1634ad48380e199081d5c82134ff9f3
Maysson <mayssonalexandre@gmai.com>
2025-05-30 16:34:39 -0400

    dispositivos

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ db = SQLAlchemy(app)
 
 class Dispositivo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.String(15), unique=True, nullable=False)
     nome = db.Column(db.String(50), nullable=False)
     local = db.Column(db.String(50), nullable=False)
-    status = db.Column(db.String(20), nullable=False)
-    mensagem = db.Column(db.String(250), nullable=False)
+    # status = db.Column(db.String(20))
+    # mensagem = db.Column(db.String(250), nullable=False)
     ultima_atualizacao = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
     ultima_sincronizacao = db.Column(db.DateTime, default=datetime.now)
 
@@ -43,6 +44,7 @@ class Mensagem_Temporaria(db.Model):
 
 class Noticia(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    dispositivo_id = db.Column(db.Integer, db.ForeignKey('dispositivo.id'), nullable=False)
     conteudo = db.Column(db.String(250), nullable=False)
     data_inicio= db.Column(db.DateTime, default=datetime.now)
     data_fim = db.Column(db.DateTime)
@@ -60,8 +62,30 @@ class Usuario(db.Model):
 
   
 with app.app_context():
+    #db.drop_all() #para limpar o banco(so nos testes)
     db.create_all()
 
 @app.route('/')
-def render_pg():
-    return None
+def show_dispositivos():
+    dispositivos = Dispositivo.query.all()
+    return render_template('dispositivos.html', dispositivos=dispositivos)
+
+@app.route('/adicionar_dispositivo', methods=["GET",'POST'])
+def adicionar_dispositivo():
+    if request.method == "POST":
+        novo_dispositivo = Dispositivo(
+            nome=request.form["nome"],
+            local=request.form["local"],
+            ip=request.form["ip"],
+            # status=request.form["status"],
+            # mensagem=request.form["mensagem"],
+            ultima_atualizacao=datetime.now(),
+            ultima_sincronizacao=datetime.now()
+        )
+        db.session.add(novo_dispositivo)
+        db.session.commit()
+        return redirect("/")
+    return render_template("adicionar_dispositivo.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
