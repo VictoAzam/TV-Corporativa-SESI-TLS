9f405ad3c1ee74d12541210b69fe0aa007d98539
Maysson <mayssonalexandre@gmai.com>
2025-05-30 15:55:14 -0400

    db criado

--- app.py
+++ app.py
@@ -1,5 +1,66 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask (__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dispositivos.db'
+
+db = SQLAlchemy(app)
+
+class Dispositivo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    local = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+    mensagem = db.Column(db.String(250), nullable=False)
+    ultima_atualizacao = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    ultima_sincronizacao = db.Column(db.DateTime, default=datetime.now)
+
+class Evento(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    dispositivo_id = db.Column(db.Integer, db.ForeignKey('dispositivo.id'), nullable=False)
+    titulo = db.Column(db.String(50), nullable=False)
+    descricao = db.Column(db.String(250), nullable=False)
+    link = db.Column(db.String(250))
+    status = db.Column(db.String(20), nullable=False)
+    data_inicio = db.Column(db.DateTime, default=datetime.now)
+    data_fim = db.Column(db.DateTime)
+    
+    dispositivo = db.relationship('Dispositivo', backref=db.backref('eventos', lazy=True))
+    
+class Mensagem_Temporaria(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    dispositivo_id = db.Column(db.Integer, db.ForeignKey('dispositivo.id'), nullable=False)
+    mensagem = db.Column(db.String(250), nullable=False)
+    link = db.Column(db.String(250), nullable=False)
+    prioridade = db.Column(db.String(20), nullable=False)
+    data_inicio= db.Column(db.DateTime, default=datetime.now)
+    data_fim = db.Column(db.DateTime)
+    hora_fim = db.Column(db.Time)
+    
+    dispositivo = db.relationship('Dispositivo', backref=db.backref('mensagens_temporarias', lazy=True))
+
+class Noticia(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    conteudo = db.Column(db.String(250), nullable=False)
+    data_inicio= db.Column(db.DateTime, default=datetime.now)
+    data_fim = db.Column(db.DateTime)
+    prioridade = db.Column(db.String(20), nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+    
+    dispositivo = db.relationship('Dispositivo', backref=db.backref('noticias', lazy=True))
+    
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    senha = db.Column(db.String(100), nullable=False)
+    data_criacao = db.Column(db.DateTime, default=datetime.now)
+
+  
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def render_pg():
